1bdafe371073ad5f136f06dd39c5e94ebcaad5ea
Eibho <eibhyor@gmail.com>
2018-12-10 14:45:47 +0000

    Shoot and Jump animations

--- GameProject/Assets/Animation/Scipts/Movement.cs
+++ GameProject/Assets/Animation/Scipts/Movement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,13 +7,18 @@ public class Movement : MonoBehaviour {
     private Animator _animator;
 
     public float MaxSpeed = 5;
+
+    private Rigidbody _rb;
+
+    
     
 
     // Use this for initialization
     void Start () {
 
-
+        _rb = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
+        
 
     }
 	
@@ -25,8 +30,11 @@ public class Movement : MonoBehaviour {
         var x = Input.GetAxis("Horizontal");
         var y = Input.GetAxis("Vertical");
 
-
+        
         Move(x, y);
+
+        Jump();
+        Shoot();
 
     }
 
@@ -39,6 +47,22 @@ public class Movement : MonoBehaviour {
 
         transform.position += Vector3.right * MaxSpeed * x * Time.deltaTime; // x = VelX
         transform.position += Vector3.forward * MaxSpeed * y * Time.deltaTime; // y = VelY
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger("Jump");
+        }
+    }
+
+    private void Shoot()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _animator.SetTrigger("Shoot");
+        }
     }
 
 }
