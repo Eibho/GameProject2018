e9206cbdce74d7e365da2f8a8bc86bc0eba0e748
Eibho <eibhyor@gmail.com>
2018-12-10 21:54:23 +0000

    NavMesh No animations

---
+++ GameProject/Assets/Scipts/EnemyController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyController : MonoBehaviour {
+
+    public float lookRadius = 10f;
+
+    Transform player;
+    NavMeshAgent navAgent;
+
+    public GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        player = playerObject.transform;
+
+        navAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            navAgent.SetDestination(player.position);
+
+            if(distance<= navAgent. stoppingDistance)
+            {
+                //Attack
+                //Face player
+                FacePlayer();
+            }
+        }
+	}
+
+    void FacePlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x , 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        //slerp - smooths movement
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+
+    }
+}
