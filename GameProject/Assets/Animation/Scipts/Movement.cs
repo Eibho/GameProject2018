416c8eade9e35b15c7aa905e139be3975c2a19e5
Eibho <eibhyor@gmail.com>
2018-12-10 14:24:06 +0000

    First Commit. Basic Character Movement

---
+++ GameProject/Assets/Animation/Scipts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Animator _animator;
+
+    public float MaxSpeed = 5;
+    
+
+    // Use this for initialization
+    void Start () {
+
+
+        _animator = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_animator == null) return;
+
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+
+        Move(x, y);
+
+    }
+
+
+    private void Move(float x, float y)
+    {
+
+        _animator.SetFloat("VelX", x);
+        _animator.SetFloat("VelY", y);
+
+        transform.position += Vector3.right * MaxSpeed * x * Time.deltaTime; // x = VelX
+        transform.position += Vector3.forward * MaxSpeed * y * Time.deltaTime; // y = VelY
+    }
+
+}
