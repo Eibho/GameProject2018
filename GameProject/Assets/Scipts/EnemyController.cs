0fbcaef390d84c04a87e14ddf088157ec7202e29
Eibho <eibhyor@gmail.com>
2018-12-12 18:33:16 +0000

    Finished Game (only one enemy)

--- GameProject/Assets/Scipts/EnemyController.cs
+++ GameProject/Assets/Scipts/EnemyController.cs
@@ -14,6 +14,7 @@ public class EnemyController : MonoBehaviour {
     private Animator _anim;
 
     public GameObject playerObject;
+    public GameObject enemyObject;
     private Movement movementScript;
 
 
@@ -68,7 +69,7 @@ public class EnemyController : MonoBehaviour {
 
     private void Destroy()
     {
-        Destroy(gameObject, 3);
+        Destroy(enemyObject, 3);
     }
 
     void FacePlayer()
