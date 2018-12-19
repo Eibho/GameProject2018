233a8c1fc268a09902c093381b626b36e786b368
Eibho <eibhyor@gmail.com>
2018-12-19 13:13:17 +0000

    The redownloaded file

---
+++ GameProject2018-master/GameProject/Assets/Scipts/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed;
+    private EnemyController enemy;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemy = FindObjectOfType<EnemyController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        Destroy(gameObject, 5);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject,0.2f);
+            Debug.Log("Hit");
+            enemy.enemyHealth = enemy.enemyHealth - 1;
+        }
+        
+    }
+}
+
+    
